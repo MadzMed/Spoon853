3abaca6da291715bd28e485473d62cd8c87f2578
mahdi LAMRIBEN <mehdilmbn@gmail.com>
2022-05-23 10:20:37 +0200

    course

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
   root to: 'pages#home'
+  resources :tasks, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
